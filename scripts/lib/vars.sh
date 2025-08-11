94c9b6d2ec67abd138c128ffdfea03a62d541ddc
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2025-08-12 00:05:21 +0100

    feat(scripts): Support .env file for loading data paths (launch.json not supported)

--- scripts/lib/vars.sh
+++ scripts/lib/vars.sh
@@ -5,6 +5,7 @@ set -eEuo pipefail
 lib_scripts_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 repo_path="$(realpath "${lib_scripts_path}/../..")"
+env_file_path="${repo_path}/.env"
 build_directory="build"
 build_path="${repo_path}/${build_directory}"
 scripts_path="${repo_path}/scripts"
