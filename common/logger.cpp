49a1a63caf1cc1fe5ed1e5ff887585f5c457705e
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2025-08-14 03:03:48 +0100

    feat(td): Logger methods and static note

--- common/logger.cpp
+++ common/logger.cpp
@@ -8,52 +8,61 @@
 const std::string_view CncLogger::DefaultLoggerName = std::string_view("nco");
 const auto CncLogger::DefaultLogger = CncLogger(CncLogger::DefaultLoggerName);
 
-void CncLogger::Register(const std::string_view name) {
-  static std::once_flag onceFlag;
+void CncLogger::Register(const std::string_view name)
+{
+    static std::once_flag onceFlag;
 
-  static std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> stdout_sink;
-  static std::shared_ptr<spdlog::sinks::rotating_file_sink_mt> rotating_sink;
-  static std::vector<spdlog::sink_ptr> sinks;
+    static std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> stdout_sink;
+    static std::shared_ptr<spdlog::sinks::rotating_file_sink_mt> rotating_sink;
+    static std::vector<spdlog::sink_ptr> sinks;
 
-  std::call_once(onceFlag, []() {
-    spdlog::init_thread_pool(8192, 1);
+    std::call_once(onceFlag, []() {
+        spdlog::init_thread_pool(8192, 1);
 
-    stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("nco.log", 1024*1024*10, 3);
-    sinks = std::vector<spdlog::sink_ptr> {stdout_sink, rotating_sink};
+        stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("nco.log", 1024 * 1024 * 10, 3);
+        sinks = std::vector<spdlog::sink_ptr>{stdout_sink, rotating_sink};
 
-    std::string logger_name = std::string(CncLogger::DefaultLoggerName.data());
+        auto logger_name = std::string(CncLogger::DefaultLoggerName);
 
-    spdlog::set_default_logger(
-      std::make_shared<spdlog::async_logger>(
-        logger_name,
-        sinks.begin(),
-        sinks.end(),
-        spdlog::thread_pool(),
-        spdlog::async_overflow_policy::block
-      )
-    );
-  });
+        spdlog::set_default_logger(std::make_shared<spdlog::async_logger>(
+            logger_name, sinks.begin(), sinks.end(), spdlog::thread_pool(), spdlog::async_overflow_policy::block));
+    });
 
-  std::string logger_name = std::string(name.data());
+    auto logger_name = std::string(name);
 
-  spdlog::register_logger(
-    std::make_shared<spdlog::async_logger>(
-      logger_name,
-      sinks.begin(),
-      sinks.end(),
-      spdlog::thread_pool(),
-      spdlog::async_overflow_policy::block
-    )
-  );
+    spdlog::register_or_replace(std::make_shared<spdlog::async_logger>(
+        logger_name, sinks.begin(), sinks.end(), spdlog::thread_pool(), spdlog::async_overflow_policy::block));
 }
 
-CncLogger::CncLogger(const std::string_view name) : Name(name) {
-  if (name != CncLogger::DefaultLoggerName) {
-    Register(name);
-  }
+CncLogger::CncLogger(const std::string_view name) : Name(name)
+{
+    if (name != CncLogger::DefaultLoggerName) {
+        Register(name);
+    }
 }
 
-void CncLogger::Log_Info(const std::string_view message) const {
-  spdlog::get(Name.data())->info(message);
+void CncLogger::Log_Info(const std::string_view message) const
+{
+    spdlog::get(std::string(Name))->info(message);
+}
+
+void CncLogger::Log_Warn(const std::string_view message) const
+{
+    spdlog::get(std::string(Name))->warn(message);
+}
+
+void CncLogger::Log_Error(const std::string_view message) const
+{
+    spdlog::get(std::string(Name))->error(message);
+}
+
+void CncLogger::Log_Debug(const std::string_view message) const
+{
+    spdlog::get(std::string(Name))->debug(message);
+}
+
+void CncLogger::Log_Trace(const std::string_view message) const
+{
+    spdlog::get(std::string(Name))->trace(message);
 }
--- common/logger.h
+++ common/logger.h
@@ -2,6 +2,10 @@
 
 #include <string_view>
 
+/**
+ * @brief Logger class using spdlog. For memory and thread safety, only assign instances
+ *        of this class using static storage specifiers; use a static member or variable.
+ */
 class CncLogger
 {
 public:
@@ -11,7 +15,10 @@ public:
     CncLogger(const std::string_view name);
 
     void Log_Info(const std::string_view message) const;
-
+    void Log_Warn(const std::string_view message) const;
+    void Log_Error(const std::string_view message) const;
+    void Log_Debug(const std::string_view message) const;
+    void Log_Trace(const std::string_view message) const;
 private:
     static CncLogger Instance;
 
